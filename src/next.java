170a875a3b06d9d8faf8c39f03f59e2d1e05de45
sammyharrison58 <sammyharrison58@gmail.com>
2026-09-15 20:56:21 +0300

    apply changes

---
+++ src/next.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Cannot divide by zero!");
+                    input.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                input.close();
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        input.close();
+    }
+}
