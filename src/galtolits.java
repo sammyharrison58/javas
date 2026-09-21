8fce9dbe5e49927ff27d9bc5259b6e6313e23548
sammyharrison58 <sammyharrison58@gmail.com>
2026-09-21 21:36:14 +0300

    apply changes

--- src/galtolits.java
+++ src/galtolits.java
@@ -3,7 +3,7 @@ public class galtolits
     public static void main(String[] args)
     {
         double galan = 10;
-        double liter = galan * 3.785;
+        double liter = galan * 3.7854;
         System.out.println("Galan in liters: " + liter);
     }
 }
